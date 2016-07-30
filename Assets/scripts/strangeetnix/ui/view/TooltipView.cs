0b51d3c7b09adb5722613f4246f9f78b570782f4
Stanislav Bereslavskiy <stanislav.bereslavskiy@nikaent.com>
2016-07-30 13:48:27 +0300

    can use tooltip (need fix tooltip child behind all)

---
+++ Assets/scripts/strangeetnix/ui/view/TooltipView.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+
+namespace strangeetnix.ui
+{
+	public class TooltipView : MonoBehaviour
+	{
+		public bool IsActive {
+			get {
+				return gameObject.activeSelf;
+			}
+		}
+		//public CanvasGroup tooltip;
+		public UnityEngine.UI.Text tooltipText;
+
+		void Awake() {
+			instance = this;
+			HideTooltip();
+		}
+
+		public void ShowTooltip(string text, Vector3 pos) {
+			if (tooltipText.text != text)
+				tooltipText.text = text;
+
+			transform.position = pos;
+
+			gameObject.SetActive(true);
+		}
+
+		public void HideTooltip() {
+			gameObject.SetActive(false);
+		}
+
+		// Standard Singleton Access 
+		private static TooltipView instance;
+		public static TooltipView Instance
+		{
+			get
+			{
+				if (instance == null)					
+					instance = GameObject.FindObjectOfType<TooltipView>();
+				return instance;
+			}
+		}
+	}
+}
+
