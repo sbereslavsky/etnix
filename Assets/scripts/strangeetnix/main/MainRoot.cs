98706f36421b38daac1aa3569c24af0f1ff50f8d
Stanislav Bereslavsky <stanislav.bereslavsky@gmail.com>
2016-08-05 14:26:04 +0300

    fix dynamic adding remote logger for android/ios

--- Assets/scripts/strangeetnix/main/MainRoot.cs
+++ Assets/scripts/strangeetnix/main/MainRoot.cs
@@ -11,6 +11,12 @@ namespace strangeetnix.main
 		{
 			//Instantiate the context, passing it this instance.
 			context = new MainContext(this);
+
+			//add remote logger for Android/IOS
+			#if !UNITY_EDITOR && (UNITY_IPHONE || UNITY_ANDROID)
+			GameObject remoteLogger = (GameObject)Instantiate (new GameObject (), Vector3.zero, Quaternion.identity);
+			remoteLogger.AddComponent<RemoteLogger> ();
+			#endif
 		}
 	}
 }
